8b09e010891acd37fe8f140f45b73e63bf578a04
John Häggerud <john.haggerud@lnu.se>
2016-02-09 15:49:01 +0100

    added demo to L02

---
+++ lectures/02/demo/routes/home.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// Use the express.Router class to create modular, mountable route handlers.
+let router = require("express").Router();
+
+// this will trigger on the root url (/)
+router.route("/")
+    .get(function(request, response) {
+        // render the view for the home
+        response.render("home/index"); 
+    });
+    
+// Exports
+module.exports = router;
