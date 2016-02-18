9afdd94b28386be4f2a6709d615e08b4d44dd38b
John Häggerud <john.haggerud@lnu.se>
2016-02-18 11:33:51 +0100

    added code for handling node exit

--- lectures/03/demo/libs/dbHelper.js
+++ lectures/03/demo/libs/dbHelper.js
@@ -5,14 +5,22 @@ let mongoose     = require("mongoose");
 module.exports = {
     initilize : function() {
       // reed db Configuration
-      var dbConfig = require("../config/database.js");
-      var db = mongoose.connection;
+      let dbConfig = require("../config/database.js");
+      let db = mongoose.connection;
 
       // mayby should remove out from this file - EventEmitters?
       db.on("error", console.error.bind(console, "connection error:"));
 
       db.once("open", function() {
         console.log("Succesfully connected to mongoDB");
+      });
+
+      // If the Node process ends, close the Mongoose connection.
+      process.on("SIGINT", function() {
+            db.close(function() {
+               console.log("Mongoose connection disconnected through app termination.");
+              process.exit(0);
+          });
       });
 
       // Connect to the database.
